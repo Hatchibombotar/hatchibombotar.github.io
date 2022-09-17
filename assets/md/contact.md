0f3c64ced3f73cafd66121a1541df213adf3f0e4
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-09-17 19:14:14 +0100

    Change to use Solidjs

--- assets/md/contact.md
+++
@@ -1,5 +0,0 @@
-## Contact
-
-Discord - hatchibombotar#3794
-
-Email - [hatchibombotar.mc@gmail.com](mailto:hatchibombotar.mc@gmail.com)
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import logo from './assets/logo.png';
+import styles from './App.module.css';
+import projects from "./projects.json"
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <div class={styles.title}>
+          <h1>Hatchibombotar</h1> <img src={logo} alt="My Logo" class={styles.logo}></img>
+        </div>
+        <p>Developer</p>
+      </header>
+      <Projects></Projects>
+      <div>
+        <h2>Contact</h2>
+        <p>- Discord - hatchibombotar#3794</p>
+        <p>- Email - <a href="mailto:hatchibombotar.mc@gmail.com">hatchibombotar.mc@gmail.com</a></p>
+      </div>
+    </div>
+  );
+}
+
+function Projects() {
+  return <>
+  <h2>Projects</h2>
+  <p>
+    <For each={projects}>{(project) =>
+      <p>
+        - 
+        <a href={project.href}>
+          {project.name}
+        </a>
+        <a> - </a> 
+        {project.description} 
+      </p>
+    }</For>
+  </p>
+  </>
+}
+
+export default App;
