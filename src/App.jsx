0e85a2b5428418bdc1b7449b417f1d00b7515eb4
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-10-09 07:50:37 +0100

    Switched to markdown homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,30 @@ import styles from './App.module.css';
 import projects from "./projects.json"
 import skills from "./skills.json"
 
+import Markdown from "solid-markdown";
+import homeContent from "./assets/homepage.md?raw"
+
+
 function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <div class={styles.title}>
-          <h1>Hatchibombotar</h1> <img src={logo} alt="My Logo" class={styles.logo}></img>
+          <div>
+            <h1>Hatchibombotar</h1>
+            <p>Developer</p>
+          </div>
+
+          <img src={logo} alt="My Logo" class={styles.logo}></img>
         </div>
-        <p>Developer</p>
+        
       </header>
-      <Projects/>
-      <Skills/>
-      <div>
-        <h2>Contact</h2>
-        <p>Discord - hatchibombotar#3794</p>
-        <p>Email - <a href="mailto:hatchibombotar.mc@gmail.com">hatchibombotar.mc@gmail.com</a></p>
+      
+      <div class={styles.markdownContainer}>
+        <Markdown>{homeContent}</Markdown>
       </div>
     </div>
   );
-}
-
-function Projects() {
-  return <>
-  <h2>Projects</h2>
-  <p>
-    <For each={projects}>{(project) =>
-      <p>
-        
-        <a href={project.href}>
-          {project.name}
-        </a>
-        <a> - </a> 
-        {project.description} 
-      </p>
-    }</For>
-  </p>
-  </>
-}
-
-function Skills() {
-  return <>
-  <h2>Skills</h2>
-  <p>
-    <For each={skills}>{(skill) =>
-      <p>
-        {skill.name}
-        <a> - </a> 
-        {skill.level} 
-      </p>
-    }</For>
-  </p>
-  </>
 }
 
 export default App;
---
+++ src/assets/homepage.md
@@ -0,0 +1,26 @@
+## Projects
+- [Minecraft Add-ons - Lots of small-scale bedrock addons](https://mcpedl.com/user/hatchibombotar/)
+
+- [Hoax Shader - Minecraft 1.16+ texture 'shaders'](https://mcpedl.com/hoax-shader/)
+
+- [Minicrafter Maker - 2D character creator](https://hatchibombotar.com/minicrafter-maker/)
+
+- [Regolith Filters - Work on regolith filters (addon modifying scripts)](https://github.com/Hatchibombotar/useful-regolith-filters)
+
+- [Other Projects - My other projects on Github](https://github.com/Hatchibombotar?tab=repositories)
+
+
+## Skills
+- JS/HTML/CSS - Advanced
+
+- SolidJS - Intermediate
+
+- Python - Intermediate
+
+- Minecraft Addons - Expert
+
+
+## Contact
+- Discord - hatchibombotar#3794
+
+- Email - [hatchibombotar.mc@gmail.com](mailto:hatchibombotar.mc@gmail.com)
